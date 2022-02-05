cb60158110b69f6d8222103858fd1d0ae2fcf76b
Corey-Derosier01 <coreyvegas12@gmail.com>
2022-02-05 12:12:48 -0500

    update index.js

--- index.js
+++ index.js
@@ -28,20 +28,6 @@ var app = {
         alert('Failed because: ' + message);
     }
 };
-window.addEventListener("batterystatus", onBatteryStatus, false);
 
-function onBatteryStatus(status) {
-    console.log("Level: " + status.level + " isPlugged: " + status.isPlugged);
-};
-window.addEventListener("batterylow", onBatteryLow, false);
-
-function onBatteryLow(status) {
-    alert("Battery Level Low " + status.level + "%");
-};
-window.addEventListener("batterycritical", onBatteryCritical, false);
-
-function onBatteryCritical(status) {
-    alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
-};
 
 app.initalize();
